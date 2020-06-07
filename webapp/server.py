7bfceafd3eb3121a46085cd2e9f1d37c9247689e
Bar Shopen <bar.shopen@gmail.com>
2020-06-07 20:31:47 +0300

    update

--- webapp/server.py
+++ webapp/server.py
@@ -1,78 +1,63 @@
 import wakkaide.network_file
 wakkaide.network_file.setup()
 from flask import Flask
-from flask import request
-import requests
-import time
-import threading
 import os
-
-# This is an example of a basic Flask server.
-# Feel free to try it for yourself!
+import mimetypes
+import json
+mimetypes.init()
 
 app = Flask(__name__)
+jsons_response = None
 
-@app.route("/")
-def index():
-  f = open('tau_smartcities/webapp/public/html/navigation_page4.html', "r")
-  return f.read(), 200, {'Content-Type': 'text/html'}
-
-@app.route("/<path:path>", methods = ['GET'])
+@app.route('/', defaults={'path': ''})
+@app.route("/<path:path>")
 def get_file(path):
+  path = get_path_in_public(path)
+  mode = "rb" if is_binary(path) else "r"
+
   try:
-    f = open('tau_smartcities/webapp/public/' + path, "rb" if is_binary(path) else "r")
-    return f.read(), 200, {'Content-Type': get_content_type(path)}
+    with open(path, mode) as f: 
+      return f.read(), 200, {'Content-Type': get_mime(path)}
+
   except FileNotFoundError:
     error_message = 'FileNotFoundError: No such file: ' + path
     print(error_message)
-    return error_message, 404, {'Content-Type': 'text/html'} 
+    return error_message, 404, {'Content-Type': 'text/html'}
 
 @app.route('/<path:path>', methods = ['POST'])
 def post(path):
   print(request)
   print(request.form)
 
-@app.route("/roads/<roadName>")
-def get_road(roadName):
-  """(BAR) this function is here for encapsulating json file requests 
-     jsons are now accessible through roads/<name_of_json>
-   """
-  file_path = "tau_smartcities/webapp/public/assets/jsons/" + roadName 
-  try:
-    with open(file_path, "r", "utf-8" ) as road_file:
-      return road_file.read(), 200, {'Content-Type': 'text/html'}
+@app.route('/jsons')
+def jsons():
+  global jsons_response
+  if not jsons_response:
+    all_jsons = []
+    for i in range(5):
+      path = get_path_in_public("jsons/{0}.json".format(i))
+      with open(path, "r",) as current_json_file:
+        all_jsons.append(json.load(current_json_file))
+    jsons_response = json.dumps(all_jsons, ensure_ascii=False), 200, {'Content-Type': get_mime(path)}
+  return jsons_response
 
-  except FileNotFoundError:
-    error_message = 'FileNotFoundError: No such file: roads/{0} '.format(roadName)
-    print(error_message)
-    return error_message, 404, {'Content-Type': 'text/html'} 
+def get_path_in_public(path:str):
+  if not path:
+    path = os.path.join(path, 'app.html')
+  
+  relative_path = os.path.relpath(os.path.dirname(__file__), '/code/root')
+  return os.path.join(relative_path, 'public', path)
 
-def get_content_type(path):
-  print(path)
-  if path.endswith('.html'):
-    return 'text/html'
-  if path.endswith('.json'):
-    return 'application/json'
-  if path.endswith('.js'):
-    return 'text/javascript'
-  if path.endswith('.txt'):
-    return 'text/plain'
-  if path.endswith('.css'):
-    return 'text/css'
-  if path.endswith('.svg'):
-    return 'image/svg+xml'
-  if path.endswith('.ico'):
-    return 'image/x-icon'
-  if path.endswith('.png'):
-    return 'image/png'
-  if path.endswith('.jpg'):
-    return 'image/jpeg'
-  if path.endswith('.gif'):
-    return 'image/gif'
+def get_mime(path):
+  extension = os.path.splitext(path)[-1]
+  return mimetypes.types_map[extension]
 
 def is_binary(path):
   _, file_extension = os.path.splitext(path)
   return file_extension in ['.ico', '.png', '.jpg', '.gif']
 
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000)
+
+
